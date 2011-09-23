42c4264cda6ded82cbcf48096b3bbef0f5c9a850
unknown <Heber@.(none)>
2011-09-23 14:16:45 -0300

    alteracoes no layout

--- session.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-session_start();
-if (isset($_SESSION['usuario']))
-    echo "Bem-vindo, " . $_SESSION['usuario'] . "!&nbsp<a href='/locacao/logout.php'>Sair</a><br/><br/>";
-else
-    die("Voce precisa estar logado para acessar esta pagina<br/><a href='/locacao/login.php'>Clique aqui</a> para logar-se.<br/>");
-?>
